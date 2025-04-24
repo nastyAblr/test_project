d38d2593b585dfdcc504b9b1badf225693e82030
Богачева Анастасия <britta18britta@gmail.ru>
2025-04-24 14:24:59 +0300

    commit1

---
+++ mainapp/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from django.urls import include
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.accommodations, name='index'),
+    #path('accommodations/<int:pk>/', mainapp.accommodations,
+         #name='accommodations'),
+    #path('accommodations/<int:pk>/page/<int:page>/', mainapp.accommodations,
+         #name='page'),
+    path('auth/', include('authapp.urls', namespace='auth')),
+    path('accommodation_details/<int:pk>/', mainapp.accommodation,
+         name='accommodation'),
+]
